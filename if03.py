f3e9866acf6c744384ec2c25f7b9e8059d7b9149
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-07-11 02:55:46 -0400

    3 done

--- if03.py
+++ if03.py
@@ -1,4 +1,11 @@
+a=0
 def main(a):
+    if a>0:
+        return a+1
+    if a<0:
+        return a-2
+    if a==0:
+        return 10
     """
     If the number is positive, increase it to 1,if the number is negative decrease it to 2.
     If it is 0, assign 10.
@@ -7,4 +14,5 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    return a
+print(main(a))
