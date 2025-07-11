9cbe464f6f0319bc46b590108836dbc0f54d9e10
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-07-11 03:15:13 -0400

    5 done

--- if05.py
+++ if05.py
@@ -1,4 +1,14 @@
+a=-2
+b=-9
+c=-1
 def main(a,b,c):
+    x=0
+    if a<0:
+        x=x+1
+    if b<0:
+        x=x+1
+    if c<0:
+        x=x+1
     """
     Find number of negative numbers there are in the given numbers.
     Args:
@@ -8,4 +18,5 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+    return x
+print (main(a,b,c))
