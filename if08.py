f43f6cdf150034d8f8a0815abd4311078e01e0e3
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-07-12 00:28:28 -0400

    8 done

--- if08.py
+++ if08.py
@@ -1,4 +1,13 @@
+a=-319
 def main(a):
+    if abs(a)//10>0 and abs(a)//10<10 and a%2==1:
+        return "two-digit odd number"
+    if abs(a)//10>0 and abs(a)//10<10 and a%2==0:
+        return "two-digit even number"
+    if abs(a)//100>0 and abs(a)//100<10 and a%2==1:
+        return "three-digit odd number"
+    if abs(a)//100>0 and abs(a)//100<10 and a%2==0:
+        return "three-digit even number"
     """
     Given an integer a, check the following conditions:
     "two-digit odd number",
@@ -11,4 +20,5 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    return a
+print (main(a)) 
